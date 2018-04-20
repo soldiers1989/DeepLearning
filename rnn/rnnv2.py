f39cf95026d4771538046446f082ce3f296a0b94
Bin Cai <caibinbupt@hotmail.com>
2018-04-20 14:32:24 +0800

    add rnnv2.py

---
+++ rnn/rnnv2.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+'''
+@author:
+BinCai (bincai@tencent.com)
+https://r2rt.com/recurrent-neural-networks-in-tensorflow-i.html
+'''
+
+"""
+Imports
+"""
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import time
+import os
+import urllib.request
+
+"""
+Load and process data, utility functions
+"""
+
+file_url = 'https://raw.githubusercontent.com/jcjohnson/torch-rnn/master/data/tiny-shakespeare.txt'
+file_name = 'tinyshakespeare.txt'
+if not os.path.exists(file_name):
+    urllib.request.urlretrieve(file_url, file_name)
+
+with open(file_name,'r') as f:
+    raw_data = f.read()
+    print("Data length:", len(raw_data))
+
+vocab = set(raw_data)
+vocab_size = len(vocab)
+idx_to_vocab = dict(enumerate(vocab))
+vocab_to_idx = dict(zip(idx_to_vocab.values(), idx_to_vocab.keys()))
+
+data = [vocab_to_idx[c] for c in raw_data]
+del raw_data
+
+def gen_epochs(n, num_steps, batch_size):
+    for i in range(n):
+        yield reader.ptb_iterator(data, batch_size, num_steps)
+
+def reset_graph():
+    if 'sess' in globals() and sess:
+        sess.close()
+    tf.reset_default_graph()
+
+def train_network(g, num_epochs, num_steps = 200, batch_size = 32, verbose = True, save=False):
+    tf.set_random_seed(2345)
+    with tf.Session() as sess:
+        sess.run(tf.initialize_all_variables())
+        training_losses = []
+        for idx, epoch in enumerate(gen_epochs(num_epochs, num_steps, batch_size)):
+            training_loss = 0
+            steps = 0
+            training_state = None
+            for X, Y in epoch:
+                steps += 1
+
+                feed_dict={g['x']: X, g['y']: Y}
+                if training_state is not None:
+                    feed_dict[g['init_state']] = training_state
+                training_loss_, training_state, _ = sess.run([g['total_loss'],
+                                                      g['final_state'],
+                                                      g['train_step']],
+                                                             feed_dict)
+                training_loss += training_loss_
+            if verbose:
+                print("Average training loss for Epoch", idx, ":", training_loss/steps)
+            training_losses.append(training_loss/steps)
+
+        if isinstance(save, str):
+            g['saver'].save(sess, save)
+
+    return training_losses
